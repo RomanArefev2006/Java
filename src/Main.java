df6de440960e500ebca5ede6b65916b7876d463c
roman <romochkaarefev@list.ru>
2024-09-30 20:43:38 +0300

    random number

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,30 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        while (true) {
-            System.out.print("Введите число в кг: ");
-            int kg = scan.nextInt();
-            int gr = kg * 1000;
-            int mgr = gr * 1000;
-            System.out.println("Кг: " + kg + "\nГр: " + gr + "\nМгр: " + mgr);
-        }
+        //Угадай число
+        Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int randomNumber = (int) (Math.random() * 100);
+        int num;
+        int count = 0;
+
+        do {
+            System.out.print("Введите число: ");
+            num = scanner.nextInt();
+            count++;
+            if (num > randomNumber) {
+                System.out.println("Загаданное число меньше");
+            } else if (num < randomNumber) {
+                System.out.println("Загаданное число больше");
+            }
+
+        }while(num != randomNumber);
+
+        System.out.println("Вы выиграли!\nБыло использовано попыток:"+ count +"\nБыло загаданно число: " + randomNumber);
+
+        // Поле чудес
+
     }
 }
