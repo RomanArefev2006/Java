87eb313c49d8f30b127e4a6e3fa10a893887e410
123 <123@>
2024-12-20 12:45:03 +0300

    oop

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,33 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        Garage garage = new Garage();
-        Car car = new Car();
-        Bicycle bicycle = new Bicycle();
-        Boat boat = new Boat();
 
-        System.out.println("Автомобиль: ");
-        garage.operateVehicle(car);
-        System.out.println();
-
-        System.out.println("Лодка: ");
-        garage.operateVehicle(boat);
-        System.out.println();
-
-        System.out.println("Велосипед: ");
-        garage.operateVehicle(bicycle);
-        System.out.println();
+    static void callPersonToDuty(Person person) {
+        person.performDuty();
     }
+
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.add(new Student("Иван", 16, 10));
+        people.add(new Teacher("Мария Ивановна", 35, "Математика"));
+        people.add(new Principal("Александр Петрович", 50, 25));
+
+        // Вызываем метод callPersonToDuty для каждого человека
+        for (Person person : people) {
+            callPersonToDuty(person);
+
+            // Вызов уникальных методов
+            if (person instanceof Student) {
+                ((Student) person).study();
+            } else if (person instanceof Teacher) {
+                ((Teacher) person).teach();
+            } else if (person instanceof Principal) {
+                ((Principal) person).manage();
+            }
+
+            System.out.println();
+        }
+    }
+
 }
